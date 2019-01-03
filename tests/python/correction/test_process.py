dc21181dfaca0928d494a84df428f80661182706
twj2417 <twj2417@163.com>
2019-01-03 09:49:23 +0800

    complete runable

---
+++ tests/python/correction/test_process.py
@@ -0,0 +1,7 @@
+from dpad.correction.energygain import energygain
+import unittest
+import pytest
+import numpy as np
+from doufo.tensor.tensor import all_close
+from dpad.correction.actual2theory import module_correction,single_correction
+import h5py
--- tests/python/test_io.py
+++
@@ -1,4 +0,0 @@
-import dpad.io import is_file_normal,load_data
-
-def test_load_data():
-    
